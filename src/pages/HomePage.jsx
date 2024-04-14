1b426005b33c996f16fd48fa9dccf43c3eacb339
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-04-14 16:13:32 +0200

    HomePage 3 sections full width

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import classes from '../styles/homePage.module.css'
+import image from '../assets/cover.jpeg'
+import videoSrc from '../assets/legal2.mp4'
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+    return ( 
+        <div className={classes.pageCtn}>
+            <div className={classes.headlineCtn}>
+                {/* <h1>Hello World</h1> */}
+                <h3>Jdjjsdjsdjsdkskdklskdlskdskdk <br/>
+                    sjddjskldjsjdsjdsjdjdsds <br/>
+                    hdshsjdsjdnsdnsdnsndsndknsdksndksdsk <br/>
+                    njsdjsndskdjskdjks
+                </h3>
+            </div>
+            <div className={classes.titleUX}>  
+                <h1>UX Design</h1>
+            </div>
+            <div className={classes.projectUXctn}>
+                <Link to='/legal'> 
+                <img src={image} alt="Project Cover" />
+                </Link>
+            </div> 
+            <div className={classes.titleMicro}>  
+                <h1>Another Project</h1>
+            </div>
+            <div className={classes.projectCtn}>
+            <img src={image} alt="Project Cover" />
+            </div> 
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/LegalPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "../styles/legal.module.css"
+
+const LegalPage = () => {
+    return ( 
+        <>
+        <h1>Hello</h1>
+        </>
+     );
+}
+ 
+export default LegalPage;
