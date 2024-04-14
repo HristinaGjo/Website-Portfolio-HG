53d3392dfd9ab821bdc19075632958f3ddec9f08
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-04-14 22:31:12 +0200

    State before adding the overlay text on the image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import coverImage from '../assets/cover.jpeg';
 
 function useParallax(value, distance) {
-  return useTransform(value, [0, 1], [-distance, distance]);
+  return useTransform(value, [0, 1], [-distance * 0.5, distance * 0.5]);
 }
 
 const HomePage = () => {
@@ -12,7 +12,7 @@ const HomePage = () => {
   const { scrollYProgress } = useScroll({ target: ref });
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
-    damping: 10,
+    damping: 30,
     restDelta: 0.001
   });
 
@@ -21,33 +21,35 @@ const HomePage = () => {
   return (
     <>
       <div className={classes.pageCtn}>
-      <div className={classes.headerCtn}>
-             
-            
-
-             <div className={classes.headlineText}>
-             {/* <h1>Hello World</h1> */}
+        <div className={classes.headerCtn}>
+          <div className={classes.headlineText}>
+            {/* <h1>Hello World</h1> */}
             {/* <h3>Hey there!<br/> */}
-             <br/>
-             <h3> I'M HRISTINA</h3> 
-             <span> <h3>DESIGNER WITH DEVELOPER'S MIND &</h3></span> 
-             <span> <h3>DEVELOPER WITH DESIGNER EYES</h3></span> 
-             <span>UX UI DESIGN |</span> {"  "}
-             <span>INTERACTION DESIGN |</span> {"  "}
-             <span>FRONT END DEVELOPMENT</span>
-             </div>
-         </div>
+            <br/>
+            <h3> I'M HRISTINA</h3> 
+            <span> <h3>DESIGNER WITH DEVELOPER'S MIND &</h3></span> 
+            <span> <h3>DEVELOPER WITH DESIGNER EYES</h3></span> 
+            <span>UX UI DESIGN |</span> {"  "}
+            <span>INTERACTION DESIGN |</span> {"  "}
+            <span>FRONT END DEVELOPMENT</span>
+          </div>
+        </div>
 
         {/* Scroll animation section */}
         <section ref={ref} className={classes.scrollSection}>
           <motion.div className={classes.imageContainer} style={{ y }}>
             <img src={coverImage} alt="Cover" />
+            <motion.h2 className={classes.textOverlay} style={{ y }}>
+              nsjdsdsdjskdskdsdkdklsklsdklkdslkdsknncxncxncnxcnnxncnxcncnx <br/>
+              nsjdsdsdjskdskdsdkdklsklsdklkdslkdsknncxncxncnxcnnxncnxcncnx
+            </motion.h2>
           </motion.div>
-          <div className={classes.textContainer}>
-            <h2>Your Text Goes Here</h2>
-            <p>Additional text description...</p>
-          </div>
         </section>
+
+        <div className={classes.textContainer}>
+          <h2>Projects</h2>
+          <p>Additional text description...</p>
+        </div>
       </div>
 
       {/* Progress bar */}
