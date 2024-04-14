479cdbb7aef4df875952a779eb389cf3dcd334dc
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-04-14 21:57:21 +0200

    first state with parallex image and text glitching

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,53 +1,59 @@
-import React from "react";
-import classes from '../styles/homePage.module.css'
-import image from '../assets/cover.jpeg'
-import videoSrc from '../assets/legal2.mp4'
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import classes from '../styles/homePage.module.css';
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import coverImage from '../assets/cover.jpeg';
 
-
+function useParallax(value, distance) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
 
 const HomePage = () => {
-    return ( 
-        <>
-        <div className={classes.pageCtn}>
-            <div className={classes.headerCtn}>
-             
-                <div className={classes.headerImg}> 
-                <img src={image} alt="Project Cover" /> 
-                </div>
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 10,
+    restDelta: 0.001
+  });
 
-                <div className={classes.headlineText}>
-                {/* <h1>Hello World</h1> */}
-               {/* <h3>Hey there!<br/> */}
-                <br/>
-                <h3> I'M HRISTINA</h3> 
-                <span> <h3>DESIGNER WITH DEVELOPER'S MIND &</h3></span> 
-                <span> <h3>DEVELOPER WITH DESIGNER EYES</h3></span> 
-                <span>UX UI DESIGN |</span> {"  "}
-                <span>INTERACTION DESIGN |</span> {"  "}
-                <span>FRONT END DEVELOPMENT</span>
-                </div>
-            </div>
-            <div className={classes.titleUX}>  
-                <h1>UX Design</h1>
-            </div>
-            <div className={classes.projectUXctn}>
-                <Link to='/legal'> 
-                {/* <video className={classes.video} autoPlay loop muted>
-            <source src={videoSrc} type="video/mp4" />
-            </video> */}
-                <img src={image} alt="Project Cover" /> 
-                </Link>
-            </div> 
-            <div className={classes.titleMicro}>  
-                <h1>Interactive Design</h1>
-            </div>
-            <div className={classes.projectCtn}>
-            <img src={image} alt="Project Cover" />
-            </div> 
-        </div>
-        </>
-    );
-}
+  const y = useParallax(scrollYProgress, 300);
+
+  return (
+    <>
+      <div className={classes.pageCtn}>
+      <div className={classes.headerCtn}>
+             
+            
+
+             <div className={classes.headlineText}>
+             {/* <h1>Hello World</h1> */}
+            {/* <h3>Hey there!<br/> */}
+             <br/>
+             <h3> I'M HRISTINA</h3> 
+             <span> <h3>DESIGNER WITH DEVELOPER'S MIND &</h3></span> 
+             <span> <h3>DEVELOPER WITH DESIGNER EYES</h3></span> 
+             <span>UX UI DESIGN |</span> {"  "}
+             <span>INTERACTION DESIGN |</span> {"  "}
+             <span>FRONT END DEVELOPMENT</span>
+             </div>
+         </div>
+
+        {/* Scroll animation section */}
+        <section ref={ref} className={classes.scrollSection}>
+          <motion.div className={classes.imageContainer} style={{ y }}>
+            <img src={coverImage} alt="Cover" />
+          </motion.div>
+          <div className={classes.textContainer}>
+            <h2>Your Text Goes Here</h2>
+            <p>Additional text description...</p>
+          </div>
+        </section>
+      </div>
+
+      {/* Progress bar */}
+      <motion.div className={classes.progress} style={{ scaleX }} />
+    </>
+  );
+};
 
 export default HomePage;
