3ebac27c9639e3d4cb3b8c7744f57f0a2cfd2630
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-04-17 00:28:44 +0200

    about section added with left and right container

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import classes from "../styles/about.module.css"
+
+
+const About = () => {
+    return ( 
+        <>
+        <div className={classes.pageCtn}>
+            <div className={classes.leftCtn}>
+                <span style={{'--i':1}}>A</span>
+                <span style={{'--i':2}}>B</span>
+                <span style={{'--i':3}}>O</span>
+                <span style={{'--i':4}}>U</span>
+                <span style={{'--i':5}}>T</span>
+                </div>
+            <div className={classes.rightCtn}>Jjandjsndjndsndsbjdsnsjn 
+            djsdnkjssndk x ndbsjdjsdjdnjsnjsnfnklfsklf jssjdsdksjdkjsjskjdlk
+            sjndsjdnsnddnsndkdnkdnskmdsmdsmdsldmsm AAAAAAAA shdijisjdsjjds
+            hsjhasjajajajkskalksk dsdjskjdskjdkssdkdkskdlksdlksdkskds ssmndsn
+            sndjsndlkdsmdklsmkdmsmdsmdslmdldmldmsmdmssdmdlms dnsdnsdnksmds
+            dnksdnksdnskmdsmdsdmsmdsdmsmdlsdlsdlsd,ls,dls,dls,dls,ds,s,,sd
+            dmskmdsmdmsmdmdsmsnddmkmdmssdmsldsdmdsmdjsndndnnmdmdmd
+            </div>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import Footer from "../components/Footer";
 
 import Header from "../components/Header";
 import Projects from "../components/Projects";
+import About from "../components/About";
 
 const HomePage = () => {
   return ( 
@@ -14,6 +15,7 @@ const HomePage = () => {
     
     <Header />
     <Projects />
+    <About />
 
     </>
    );
