91d63e4732348da4bd1112a72cb2d480e32b973a
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-04-20 17:08:54 +0200

    try

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-/*import ReactDOM from 'react-dom/client'*/
-import { render } from "react-dom";
+import ReactDOM from 'react-dom/client'
+/*import { render } from "react-dom"; */
 
 import App from './App.jsx'
 import './index.css'
@@ -9,7 +9,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 const container = document.getElementById("root");
 render(
   <React.StrictMode>
-    <Router>
+    <Router> 
     <App />   
     </Router>
   </React.StrictMode>,
