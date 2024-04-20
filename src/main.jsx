6fce5ee92079bd14cbfedfd03a17ca1779cd9fb0
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-04-20 16:58:14 +0200

    changes in main jsx downgrade raect

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+/*import ReactDOM from 'react-dom/client'*/
+import { render } from "react-dom";
+
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container = document.getElementById("root");
+render(
   <React.StrictMode>
     <Router>
-    <App />
+    <App />   
     </Router>
   </React.StrictMode>,
-)
+  container
+);
