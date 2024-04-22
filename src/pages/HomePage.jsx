74c1bda482edc817f1a3457bf63f37a22ca4a0f4
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-04-22 15:55:56 +0200

    trying locomotive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import locomotiveScroll from 'locomotive-scroll';
 import classes from '../styles/homePage.module.css'
 import imageOne from '../assets/me.png'
 import imageTwo from '../assets/me.png'
+import Footer from '../components/Footer.jsx'
 
 const HomePage = () => {
   const scrollRef = useRef(null);
@@ -14,50 +15,37 @@ const HomePage = () => {
     const scroll = new locomotiveScroll({
       el: scrollRef.current,
       smooth: true,
+      smartphone: {
+        smooth: true,
+        smoothMobile: 0.5 // Adjust the scroll speed for mobile
+      }
     });
-    console.log("locomotiveScroll instance initialized:", scroll);
+   /* console.log("locomotiveScroll instance initialized:", scroll); */
 
     // Destroy the scroll instance when the component unmounts
     return () => {
-      console.log("Destroying locomotiveScroll instance...");
+      /* console.log("Destroying locomotiveScroll instance..."); */
       scroll.destroy();
-      console.log("locomotiveScroll instance destroyed");
+      /* console.log("locomotiveScroll instance destroyed"); */
     };
   }); // Empty dependency array to run effect only once
 
   return (
-    <LocomotiveScrollProvider watch={[]} containerRef={scrollRef}>
+  
       <SectionContainer>
         <div data-scroll-container className={classes.pageCtn} ref={scrollRef}>
-          <section data-scroll-section>
-            <div className={classes.headerCtn}  >
-              <h3 data-scroll data-scroll-speed={2} >Hello world! I am Hristina Gjorgieva
-                 Hello world! I am Hristina Gjorgieva
-                 Hello world! I am Hristina Gjorgieva
-                 Hello world! I am Hristina Gjorgieva
-                 Hello world! I am Hristina Gjorgieva
-                 Hello world! I am Hristina Gjorgieva</h3>
+          
+            <div data-scroll-section className={classes.headerCtn}  >
+              <h1 data-scroll data-scroll-speed={4}>
+                DIGITAL DESIGNER
+                 </h1>
             </div>
-          </section>
+        
 
-          <section data-scroll-section>
-            <div className={classes.projectsCtn}>
-              <div className={classes.projectOne} data-scroll data-scroll-speed={1}
-               data-scroll-position="top"
-               data-scroll-direction="vertical">
-                <img src={imageOne} alt="Project One" />
-                <h3>LEGAL</h3>
-              </div>
-              <div className={classes.projectTwo} data-scroll data-scroll-speed={1}
-               data-scroll-position="top"
-               data-scroll-direction="vertical">
-                <h3>LEGAL</h3>
-                <img src={imageTwo} alt="Project Two" />
-              </div>
-            </div>
-          </section>
+        
+  
 
-          <section data-scroll-section>
+      
             <div className={classes.aboutCtn}
             >
               <h3 >ABOUT</h3>
@@ -69,9 +57,9 @@ const HomePage = () => {
                mdxsdmksldmskdmdmsdmsmdskmdkmskdmsmdsmdmsmdsmdsdmsdmdksm
               </p>
             </div>
-          </section>
+    
 
-          <section data-scroll-section>
+        
             <div className={classes.servicesCtn}>
               <h3 >SERVICES</h3>
               <p>
@@ -82,19 +70,19 @@ const HomePage = () => {
                mdxsdmksldmskdmdmsdmsmdskmdkmskdmsmdsmdmsmdsmdsdmsdmdksm
               </p>
             </div>
-          </section>
 
-          <section data-scroll-section>
+
+      
             <div className={classes.connectCtn} >
               <h3>LET'S CONNECT</h3>
               <p>
                 // Your CONNECT content here
               </p>
             </div>
-          </section>
+            <Footer />
         </div>
+    
       </SectionContainer>
-    </LocomotiveScrollProvider>
   );
 };
 
