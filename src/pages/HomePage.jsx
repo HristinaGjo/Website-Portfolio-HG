13f9ee55c995e5ce20ba7f88d87125b6870f53ee
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-04-23 00:38:20 +0200

    small css fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
       <div className={classes.aboutCtn}>
         <div className={classes.text}> 
         <h2>About</h2>
-        <p>hfhfdjfkdjkfdjkdjfkjdkfjkdjdkfjjfdkjfkjd hfhfdjfkdjkfdjkdjfkjdkfjkdjdkfjjfdkjfkjd
+        <p>hfhfdjfkdjkfdjkdjfkjdkfjkdjdkfjjfdkjf
         hfhfdjfkdjkfdjkdjfkjdkfjkdjdkfjjfdkjfkjd
         hfhfdjfkdjkfdjkdjfkjdkfjkdjdkfjjfdkjfkjd
         hfhfdjfkdjkfdjkdjfkjdkfjkdjdkfjjfdkjfkjd
@@ -79,54 +79,3 @@ const HomePage = () => {
  
 export default HomePage;
 
-
-
-
-// HomePage.js
-/* import React, { useRef } from "react";
-import SectionContainer from "../components/SectionContainer";
-import Header from "../components/Header";
-import Projects from "../components/Projects";
-import About from "../components/About";
-import Navbar from "../components/Navbar";
-import Services from "../components/Services";
-import Footer from "../components/Footer";
-import Connect from "../components/Connect";
-
-const HomePage = () => {
-  const headerRef = useRef(null);
-  const projectsRef = useRef(null);
-  const aboutRef = useRef(null);
-
-  const scrollToRef = (refName) => {
-    switch (refName) {
-      case "headerRef":
-        headerRef.current.scrollIntoView({ behavior: "smooth" });
-        break;
-      case "projectsRef":
-        projectsRef.current.scrollIntoView({ behavior: "smooth" });
-        break;
-      case "aboutRef":
-        aboutRef.current.scrollIntoView({ behavior: "smooth" });
-        break;
-      default:
-        break;
-    }
-  };
-
-  return (
-    <>
-    <SectionContainer>
-      <Navbar scrollToRef={scrollToRef} />
-        <Header ref={headerRef} />
-        <Projects ref={projectsRef} />
-        <About ref={aboutRef} />
-        <Services />
-        <Connect />
-      </SectionContainer>
-      <Footer />
-    </>
-  );
-};
-
-export default HomePage; */
