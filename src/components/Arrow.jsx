021c0b954fb6b159317e6c6e1d61227c82f5d66e
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-05-09 11:06:52 +0200

    smaller arrows for mobile and laptop screen

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -2,24 +2,21 @@ import React from "react";
 
 const Arrow = () => {
   return (
-    <>
-      <div>
-        <svg width="125" height="100">
-
-            {/* Vertical line */}
-            <line x1="50" y1="20" x2="50" y2="40" stroke="#6a6a6a" strokeWidth="1" />
-            {/* Horizontal line */}
-            <line x1="50" y1="40" x2="100" y2="40" stroke="#6a6a6a" strokeWidth="1" />
-            {/* Arrowhead */}
-            <polygon points="100,35 110,40 100,45" fill="#6a6a6a" />
-      
-        </svg>
-      </div>
-    </>
+    <div>
+      <svg width="100" height="100">
+        {/* Vertical line */}
+        <line x1="50" y1="20" x2="50" y2="40" stroke="#6a6a6a" strokeWidth="1" />
+        {/* Shorter horizontal line */}
+        <line x1="50" y1="40" x2="70" y2="40" stroke="#6a6a6a" strokeWidth="1" />
+        {/* Arrowhead */}
+        <polygon points="70,35 80,40 70,45" fill="#6a6a6a" />
+      </svg>
+    </div>
   );
 }
 
 export default Arrow;
+
 
 
 
--- src/components/ArrowMobile.jsx
+++ src/components/ArrowMobile.jsx
@@ -1,28 +1,19 @@
 import React from "react";
 
-
-const ArrowMobile = () => {
-    return ( 
-        <>
-    
-          <svg width="80" height="50">
-            <g transform="scale(0.6)">
-          {/* Vertical line */}
-          <line x1="20" y1="10" x2="20" y2="40" stroke="#6a6a6a" strokeWidth="1" />
-          {/* Horizontal line */}
-          <line x1="20" y1="40" x2="70" y2="40" stroke="#6a6a6a" strokeWidth="1" />
-          {/* Arrowhead */}
-          <polygon points="70,35 80,40 70,45" fill="#6a6a6a" />
-          </g>
-          </svg>
-  
-        </>
-     );
+const Arrow = () => {
+  return (
+    <div>
+      <svg width="90" height="80">
+        {/* Vertical line */}
+        <line x1="50" y1="20" x2="50" y2="40" stroke="#6a6a6a" strokeWidth="1" />
+        {/* Shorter horizontal line */}
+        <line x1="50" y1="40" x2="70" y2="40" stroke="#6a6a6a" strokeWidth="1" />
+        {/* Arrowhead */}
+        <polygon points="70,35 80,40 70,45" fill="#6a6a6a" />
+      </svg>
+    </div>
+  );
 }
- 
-export default ArrowMobile;
 
-
-
-
+export default Arrow;
 
