d72e1e6d49be4ac8859c6dbdeb25b5971678fb3d
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-05-14 13:00:10 +0200

    aboutPage before pictures and branch off

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -85,24 +85,24 @@ const AboutPage = () => {
                 <div className={classes.skillsText}>
                     <div className={classes.designStrategy}>
                         <span>Design & Strategy</span>
-                        <ul className={classes.list}>
-                            <li>Design Thinking Methods</li>
-                            <li>User Research (User Testing, Surveys & Interviews)</li>
+                        <div className={classes.list}>
+                            <li>User Research (Surveys & Interviews)</li>
                             <li>User Personas & User flows</li>
                             <li>Wireframing & Prototyping</li>
+                            <li>User Testing</li>
                             <li>Visual & Interaction Design</li>
                             {/* <li>Design System</li> */}
-                        </ul>
+                        </div>
                     </div>
                      <div className={classes.frontend}>
                         <span>Frontend Development</span>
-                        <ul className={classes.list}>
+                        <div className={classes.list}>
                             <li>HTML</li>
                             <li>CSS</li>
                             <li>React.js</li>
                             <li>JavaScript</li>
                             <li>Node.js</li>
-                        </ul>
+                        </div>
                     </div>
                     
                 </div>
@@ -119,3 +119,24 @@ const AboutPage = () => {
 }
  
 export default AboutPage;
+
+
+
+
+/* <ul className={classes.list}>
+<li>Design Thinking Methods</li>
+<li>User Research (User Testing, Surveys & Interviews)</li>
+<li>User Personas & User flows</li>
+<li>Wireframing & Prototyping</li>
+<li>Visual & Interaction Design</li>
+
+</ul>
+
+
+<ul className={classes.list}>
+<li>HTML</li>
+<li>CSS</li>
+<li>React.js</li>
+<li>JavaScript</li>
+<li>Node.js</li>
+</ul> */
