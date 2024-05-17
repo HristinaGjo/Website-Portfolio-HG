2426f311bc305d3ff20b88e640850ba6fdf18e1c
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-05-17 11:44:04 +0200

    added solution section horizontal scrollbar

--- src/pages/LegalPage.jsx
+++ src/pages/LegalPage.jsx
@@ -7,11 +7,19 @@ import solutionFour from "../assets/solution4.png"
 
 
 
-const solutions = [
+/* const solutions = [
     {id:1, text:"Get Expert Advices in Specific Fields", imgSrc:solutionOne},
     {id:2, text:"Share Your Legal Concerns", imgSrc:solutionTwo},
-    {id:3, text:"Easily Share & Upload Relevant Documents", imgSrc:solutionOne},
+    {id:3, text:"Share & Upload Relevant Documents", imgSrc:solutionOne},
     {id:4, text:"Expert Advice At Your Fingertips", imgSrc:solutionFour},
+];*/
+
+const solutions = [
+    {id: 1, title: "Labour and Social Law", text: "01 - Get Expert Advices in Specific Fields", imgSrc: solutionOne},
+    {id: 2, title: "Legal Issues", text: "02 - Share Your Legal Concerns", imgSrc: solutionTwo},
+    {id: 3, title: "Documentation", text: "03 - Share & Upload Relevant Documents", imgSrc: solutionOne},
+    {id: 4, title: "Match", text: "04 - Find Your Perfect Match", imgSrc: solutionFour},
+    {id: 5, title: "Expert Advice", text: "05 - Expert Advice At Your Fingertips", imgSrc: solutionFour},
 ];
 
 
@@ -24,14 +32,14 @@ const LegalPage = () => {
             <div className={classes.legalHeroCtn}>
                 <span>DIGITAL BRIDGE BETWEEN A LAWYER AND A CITIZEN</span>
                 <h1>Legal app</h1>
+                <span>UX & UI CASE STUDY</span>
             </div>
 
             <div className={classes.overviewText}>
                 <div className={classes.textCtn}>
                 <span>ABOUT</span>
                 <p>Legal is a project which I worked on during my intensive UX & UI Design bootcamp
-                     at CareerFoundry in 2022. The project covered user & market research, ideation, 
-                     prototyping & wireframing, user testing and UI design.</p>
+                at CareerFoundry in 2022.</p>
                 </div>
 
                 <div className={classes.textCtn}>
@@ -41,9 +49,27 @@ const LegalPage = () => {
 
                 <div className={classes.textCtn}>
                 <span>ROLE</span>
-                <p>User Research <br/> Information Architecture <br/> UX Design<br/> UI Design</p>
+                <p>User Research <br/> Information Architecture <br/> UX & UI Design</p>
                 </div>
+            </div>
 
+            <div className={classes.solutionCtn}>
+                <div className={classes.solutionTitle}>
+                <span>SOLUTION APPROACH</span>
+                <h2 className={classes.gradientText}>Expert advice is  labour and social law</h2>
+                </div>
+                <div className={classes.carousel}>
+                    {solutions.map (solution=> (
+                <div className={classes.item} key={solution.id}>
+                    <div className={classes.itemCtn} style={{backgroundColor:solution.color}}>
+                    <span className={classes.itemSpam}>{solution.text}</span>
+                        <h3 className={classes.itemH2}>{solution.title}</h3>
+                        <img className={classes.imageCtn} src={solution.imgSrc}/>
+                    </div>
+                    </div>
+                    ))}
+                </div>
+                <div className={classes.solutionTitle}></div>
             </div>
 
         </div>
@@ -53,3 +79,8 @@ const LegalPage = () => {
 }
  
 export default LegalPage;
+
+
+
+/* The project covered user & market research, ideation, 
+                     prototyping & wireframing, user testing and UI design */
