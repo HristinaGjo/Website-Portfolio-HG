f36f5862dc60f5c55e8a7c17712ad51c2d432d67
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-05-28 14:54:43 +0200

    changing connect to email

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -22,7 +22,7 @@ const Connect = () => {
             <div className={classes.contact}>
             <button 
             className={classes.button} 
-            onClick={handleEmailClick}>Connect</button>
+            onClick={handleEmailClick}>hgjorieva@gmail.com</button>
             </div>
             </div>
            
