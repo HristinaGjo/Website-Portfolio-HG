67501ee6b6c6b5320ff440ae12d556b79f0a096c
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-05-29 19:53:10 +0200

    deleted comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,42 +48,5 @@ export default Navbar; */
 
 
 
-// Navbar scrolling event /changing background color when scrolling or color of the text
 
-/* import React, { useRef, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import classes from "../styles/navbar.module.css";
 
-const Navbar = ({ scrollToRef }) => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  return (
-    <div className={`${classes.navCtn} ${isScrolled ? classes.scrolled : ""}`}>
-      <div className={classes.leftCtn}>
-        <p onClick={() => scrollToRef("headerRef")}>Hristina Gjorgieva</p>
-      </div>
-      <div className={classes.rightCtn}>
-        <p onClick={() => scrollToRef("aboutRef")}>About</p>
-        <p onClick={() => scrollToRef("projectsRef")}>Work</p>
-      </div>
-    </div>
-  );
-};
-
-export default Navbar; */
